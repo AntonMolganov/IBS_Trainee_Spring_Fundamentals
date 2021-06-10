3e97cbe2c8577817c8e847a4b2def187ab724987
Anton Molganov <amolganov@ibs.ru>
2021-06-10 16:07:12 +0500

    mvc returned to initial stage

--- src/main/java/ru/ibs/trainee/spring/mvc/Controller.java
+++ src/main/java/ru/ibs/trainee/spring/mvc/Controller.java
@@ -1,23 +1,15 @@
 package ru.ibs.trainee.spring.mvc;
 
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 @org.springframework.stereotype.Controller
 @RequestMapping("/mvc")
 public class Controller {
 
     @GetMapping("example")
-    public String getExample() {
+    public String example() {
         return "example";
     }
 
-    @GetMapping("add")
-    public String getExample(@RequestParam Integer value, Model model) {
-        EntryStorage.getEntries().add(new Entry(value));
-        System.out.println("Added value="+value);
-        return "example";
-    }
 }
